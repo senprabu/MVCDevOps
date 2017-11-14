6b88bf279b6e9c84b9cc2d3a96b4da91297de2b8
senprabu <prabhu.senthil@gmail.com>
2017-11-14 15:46:32 +0800

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCDevOps.Startup))]
+namespace MVCDevOps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
